9ec8d433b520b9a740bc41b7d0f1e571d4341ba9
WaynegWeaver <71033499+WaynegWeaver@users.noreply.github.com>
2021-03-07 19:07:05 -0700

    Update MainBase_Transreceiver.ino

--- MainBase_Transreceiver/MainBase_Transreceiver.ino
+++ MainBase_Transreceiver/MainBase_Transreceiver.ino
@@ -21,7 +21,7 @@ int Index_position = 3; // Initial Value
 int Ring_position = 3; // Initial Value
 //LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) 
 
-RF24 radio(9,10); // Connected to CNS, CE Pins from transreceiver
+RF24 radio(9,10); // Connected to CE, CSN Pins from transreceiver
 
 void setup() {
   Serial.begin(9600);
